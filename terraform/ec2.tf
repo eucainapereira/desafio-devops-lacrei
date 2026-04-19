ca154b27ddc1e7a8b7632ee604a000591c3ecda1
Cainã Pereira <118144700+eucainapereira@users.noreply.github.com>
2026-04-18 23:36:43 -0300

    Change IAM instance profile reference to name

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -73,7 +73,7 @@ resource "aws_instance" "app_server" {
   
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-  iam_instance_profile        = aws_instance_profile.ec2_profile.id
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
   associate_public_ip_address = true
 
   user_data = <<-EOF
